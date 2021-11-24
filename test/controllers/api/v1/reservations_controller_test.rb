5bea2b25d52ac3ebefb490c5ad2264ed5d9a69f8
Chris Drit <ChrisDrit@gmail.com>
2021-11-24 11:57:06 -0800

    Adding stubbed out tests. Did not have time to start/complete these.

---
+++ test/controllers/api/v1/reservations_controller_test.rb
@@ -0,0 +1,5 @@
+require "test_helper"
+
+class Api::V1::ReservationsControllerTest < ActionDispatch::IntegrationTest
+  # TODO: add testing!
+end
---
+++ test/models/reservation/airbnb_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class Reservation::AirbnbTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
